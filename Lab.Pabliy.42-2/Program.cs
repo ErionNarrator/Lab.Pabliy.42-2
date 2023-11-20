cc28acf6a47ad0705e33f1a9f2af2844a03dea4c
Asus <Asus@DESKTOP-1IKSUBO>
2023-11-20 18:52:07 +0200

    Добавьте файлы проекта.

---
+++ Lab.Pabliy.42-2/Program.cs
@@ -0,0 +1,14 @@
+﻿// 4.2 слож
+int[,] mas1 = new int[10, 10];
+Random r = new Random();
+for (int i = 0; i < 10; i++)
+{
+    for (int j = 0; j < 10; j++)
+    {
+        mas1[i, j] = r.Next(-20, 20);
+        if (mas1[i, j] < 0) mas1[i, j] = (mas1[i, j] * -1) * 3;
+        else if (mas1[i, j] > 0) mas1[i, j] = (mas1[i, j] * -1) * 3;
+        Console.Write(mas1[i, j] + " ");
+    }
+    Console.WriteLine();
+}
